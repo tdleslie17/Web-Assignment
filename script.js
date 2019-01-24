8817f858fabd9c78899e745566d26c23214f1f61
Tim Leslie <tdleslie1@gmail.com>
2019-01-23 18:32:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,11 @@ if (pageBody.id == 'mainPg') {
 if (pageBody.id == "contactPg") {
     console.log("I'm on the contact page now!");
 
-    var newPara  = document.createElement('p');
-    var newText  = document.createTextNode('Here is some more text.');
     var newText1 = document.createTextNode('Image.');
     var newText2 = document.createTextNode('Description.');
     var newText3 = document.createTextNode('Here is some more text.');
 
-    newPara.appendChild(newText);
+
     var newTable = document.createElement('table');
     var newHeader1   = document.createElement('th');
     var newHeader2   = document.createElement('th');
@@ -37,42 +35,20 @@ if (pageBody.id == "contactPg") {
     var imgArr   = [newImg1, newImg2, newImg3];
     var desArr   = ['Turks and Caicos', 'Bahamas', 'Greece'];
 
-    var newRow2   = document.createElement('tr');
-    var newData3  = document.createElement('td');
-    var newData4  = document.createElement('td');
-    newData3.appendChild(newImg1);
-    newData4.textContent = desArr[0];
-    newRow2.appendChild(newData3);
-    newRow2.appendChild(newData4);
-    newTable.appendChild(newRow2);
-
-    var newRow3   = document.createElement('tr');
-    var newData4  = document.createElement('td');
-    var newData5  = document.createElement('td');
-    newData4.appendChild(newImg2);
-    newData5.textContent = desArr[1];
-    newRow3.appendChild(newData4);
-    newRow3.appendChild(newData5);
-    newTable.appendChild(newRow3);
-
-    var newRow4   = document.createElement('tr');
-    var newData6  = document.createElement('td');
-    var newData7  = document.createElement('td');
-    newData6.appendChild(newImg3);
-    newData7.textContent = desArr[2];
-    newRow4.appendChild(newData6);
-    newRow4.appendChild(newData7);
-    newTable.appendChild(newRow4);
-   
+    for (var i = 0; i < 3; i++) {
+        var newRow   = document.createElement('tr');
+        var newData1  = document.createElement('td');
+        var newData2  = document.createElement('td');
+        newData1.appendChild(imgArr[i]);
+        newData2.textContent = desArr[i];
+        newRow.appendChild(newData1);
+        newRow.appendChild(newData2);
+        newTable.appendChild(newRow);          
+    }
+     
     var divElem = document.getElementsByTagName('div')[0];
     divElem.appendChild(newTable);
-
-
 }
-
-
-
-
 
 if (pageBody.id == 'regPg') {
 
